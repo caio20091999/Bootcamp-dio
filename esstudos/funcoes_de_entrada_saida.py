98909a346e654641f5762a5df98e49174f0c3b0d
caio20091999 <caioandrade0920@gmail.com>
2024-03-19 19:10:09 -0300

    att curso python

---
+++ esstudos/funcoes_de_entrada_saida.py
@@ -0,0 +1,4 @@
+nome = str(input("Qual é o seu nome?"))
+idade = int(input("Qual é a sua idade?"))
+peso =int(input("Qual é o seu peso?"))
+print("olá meu nome é ", nome , "tenho", idade, "anos", "e estou pesando", peso, "kg")
---
+++ esstudos/variaveis_constante.py
@@ -0,0 +1,12 @@
+nome = "Caio"
+idade = 24
+
+
+nome = "Tai"
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRASILIAN_STATES = ["BA", "SP", "RJ"] 
+
+print(BRASILIAN_STATES)
